de06e1d1267331ff64ccd6074de3ab1749ee07a0
NORMss <serega.b301@gmail.com>
2024-12-25 15:49:30 +0700

    feat: EventViewModel is covered with unit tests

--- app/src/test/java/com/eltex/androidschool/model/TestEvent.kt
+++ app/src/test/java/com/eltex/androidschool/model/TestEvent.kt
@@ -1,30 +1,36 @@
 package com.eltex.androidschool.model
 
-import com.eltex.androidschool.domain.model.Post
+import com.eltex.androidschool.domain.model.Event
+import com.eltex.androidschool.domain.model.EventType
 import kotlinx.datetime.Instant
 
-data class TestPost(
+data class TestEvent(
     val id: Long,
     val author: String = "Test Author",
     val content: String = "Test Content",
     val published: Instant = Instant.fromEpochMilliseconds(0),
+    val datetime: Instant = Instant.fromEpochMilliseconds(0),
     val likedByMe: Boolean = false,
+    val participatedByMe: Boolean = false,
 ) {
-    fun toDomainPost(): Post = Post(
+    fun toDomainEvent(): Event = Event(
         id = id,
         authorId = 1L,
         author = author,
         authorJob = null,
         authorAvatar = null,
         content = content,
+        datetime = datetime,
         published = published,
         coords = null,
-        link = null,
-        mentionIds = emptySet(),
-        mentionedMe = false,
+        type = EventType.ONLINE,
         likeOwnerIds = if (likedByMe) setOf(1L) else emptySet(),
         likedByMe = likedByMe,
+        speakerIds = emptySet(),
+        participantsIds = if (participatedByMe) setOf(1L) else emptySet(),
+        participatedByMe = participatedByMe,
         attachment = null,
-        users = emptyMap()
+        link = null,
+        users = emptyMap(),
     )
 }
