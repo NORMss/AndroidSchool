27d564f3863426c4a3352fa095706c8f2a456894
NORMss <serega.b301@gmail.com>
2025-01-01 22:59:14 +0700

    test: new tests

--- app/src/test/java/com/eltex/androidschool/view/fragment/editevent/EditEventViewModelTest.kt
+++ app/src/test/java/com/eltex/androidschool/view/fragment/editevent/EditEventViewModelTest.kt
@@ -1,3 +1,47 @@
 package com.eltex.androidschool.view.fragment.editevent
 
-class EditEventViewModelTest
+import com.eltex.androidschool.TestSchedulersProvider
+import com.eltex.androidschool.domain.repository.EventRepository
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class EditEventViewModelTest {
+    @Test
+    fun setTextTest() {
+        val eventRepository = object : EventRepository {}
+        val viewModel = EditEventViewModel(
+            eventRepository = eventRepository,
+            schedulersProvider = TestSchedulersProvider,
+            eventId = 1L,
+        )
+
+        val testText = "test"
+
+        viewModel.setText(testText)
+
+        val equals = testText
+        val result = viewModel.state.value.event.content
+
+        assertEquals(equals, result)
+    }
+
+    @Test
+    fun editEventTest() {
+        val eventRepository = object : EventRepository {}
+        val viewModel = EditEventViewModel(
+            eventRepository = eventRepository,
+            schedulersProvider = TestSchedulersProvider,
+            eventId = 1L,
+        )
+        val testText = "test"
+
+        viewModel.setText(testText)
+
+        viewModel.editEvent()
+
+        val equals = testText
+        val result = viewModel.state.value.event.content
+
+        assertEquals(equals, result)
+    }
+}
